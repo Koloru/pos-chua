96b98406da5d0aeae1ee07afbfa958018a119fb7
Koloru <sos.llyr@gmail.com>
2022-11-01 14:25:01 +0800

    fix footer responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import { BsFillCartFill, BsFillCartXFill } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
 
 // Categories
-import Data  from "../../../backend/category.json";
+import Data from "../../../backend/category.json";
 import Router, { useRouter } from "next/router";
 
 export const Footer = () => {
@@ -15,10 +15,10 @@ export const Footer = () => {
 	const router = useRouter();
 	return (
 		<div className='flex justify-between w-full fixed bottom-0 bg-[#3e517a] px-5  text-white'>
-			<div className='flex flex-wrap gap-10 -mb-px text-md  text-center'>
+			<div className='flex gap-6 sm:gap-10 -mb-px text-md  text-center text-[14px] overflow-auto scrollbar-hide'>
 				<Link href='/'>
 					<button
-						className={`p-4 ${
+						className={`sm:p-4 p-2 ${
 							router.asPath === "/"
 								? "border-[#A8E0FF] font-bold"
 								: "border-transparent"
@@ -29,13 +29,13 @@ export const Footer = () => {
 				</Link>
 
 				{Data.category.map((category) => (
-					<Link key={category+'1'} href={`/category/${category}`} >
+					<Link key={category + "1"} href={`/category/${category}`}>
 						<button
-							className={`p-4 border-t-4  ${
+							className={`sm:p-4 p-2 border-t-4  ${
 								router.asPath === "/category/" + category
 									? "border-[#A8E0FF] font-bold"
 									: "border-transparent"
-							} hover:border-[#A8E0FF] hover:text-gray-300 capitalize`}
+							} hover:border-[#A8E0FF] hover:text-gray-300 capitalize text-[14px] `}
 						>
 							{category}
 						</button>
@@ -44,14 +44,16 @@ export const Footer = () => {
 			</div>
 
 			{/* Cart Button */}
-			<div className='group hover:bg-white p-4 border-t-4 border-transparent cursor-pointer'>
-				<button
-					onClick={() => setshowCart(!showCart)}
-					className='text-white text-2xl p-1 rounded duration-300 group-hover:text-[#3e517a]'
-				>
+			<button
+				onClick={() => setshowCart(!showCart)}
+				className={`flex justify-center  ${
+					showCart ? "items-end" : "items-center"
+				} group hover:bg-white p-4 border-t-4 border-transparent cursor-pointer`}
+			>
+				<div className='text-white text-2xl p-1 rounded duration-300 group-hover:text-[#3e517a]'>
 					{showCart ? <BsFillCartXFill /> : <BsFillCartFill />}
-				</button>
-			</div>
+				</div>
+			</button>
 			<Sidebar visible={showCart} />
 		</div>
 	);
