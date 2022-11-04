9edaf6ed5fa236cd4eb49fa2c82f1c6daf661446
Koloru <sos.llyr@gmail.com>
2022-11-04 21:49:24 +0800

    add key

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -8,6 +8,7 @@ export const Menu = () => {
 		<div className='flex gap-2 flex-wrap overflow-auto justify-center py-20'>
 			{items.map((item) => (
 				<MenuItem
+					key={item.name}
 					category={item.category}
 					name={item.name}
 					price={item.price}
